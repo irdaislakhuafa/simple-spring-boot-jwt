630ea0b126dee92f39fa2b241e3b16849e96032a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-24 04:57:19 +0700

    define base service

--- src/main/java/com/irdaislakhuafa/simplespringbootjwt/security/SecurityConfiguration.java
+++ src/main/java/com/irdaislakhuafa/simplespringbootjwt/security/SecurityConfiguration.java
@@ -1,12 +1,16 @@
 package com.irdaislakhuafa.simplespringbootjwt.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-// @Configuration
+import lombok.RequiredArgsConstructor;
+
+@Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
